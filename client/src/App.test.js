d786ae05e66281a6073b0f4f930e5f318fe583c6
Fresco Fresco <fresco@Frescos-MacBook-Pro.local>
2019-10-04 13:37:29 -0400

    unit testing complete, final commit for MVP

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Header from "./components/Header"
+import PlayerCard from "./components/PlayerCard"
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+test('component renders without crashing,', () => {
+  ReactDOM.render(<Header />)
+})
+
+test('playercard renders without crashing', () => {
+  ReactDOM.render(<PlayerCard />)
+})
